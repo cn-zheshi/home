cef179cd37b4297828e1cd60398de01a8864128c
cn-zheshi <1760864645@qq.com>
2020-02-24 09:45:16 +0800

    monitor注释

--- 算法天梯/monitor.cpp
+++ 算法天梯/monitor.cpp
@@ -12,6 +12,7 @@ int main(){
         counts=0;
         countn=0;
         int i,j;
+        //counts统计顺时针数的时候中间被踢掉的人，保证到最后数出的确实是k个人
         for(i=1;i<=k+counts;++i){
             int temp=sh+i>n?((sh+i)%n==0?n:(sh+i)%n):sh+i;
             if(a[temp]==0){
@@ -19,6 +20,7 @@ int main(){
             }
         }
         sh=sh+k+counts>n?((sh+k+counts)%n==0?n:(sh+k+counts)%n):sh+k+counts;
+        //countn统计逆时针数的时候中间被踢掉的人，保证到最后数出的确实是m个人
         for(int j=1;j<=m+countn;++j){
             int temp=ni-j<=0?(ni-j)%n+n:ni-j;
             if(a[temp]==0){
